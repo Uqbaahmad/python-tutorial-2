c2c1f9d6a1b7c6244a1175b2376ef7a6b426de64
Uqbaahmad <uqba2199@gmail.com>
2022-06-30 18:02:55 +0530

    first commit

---
+++ ex8.py
@@ -0,0 +1,11 @@
+from sys import argv
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("Your first variable is: ", first)
+print("Your second variable is: ", second)
+print("Your third variable is: ", third)
+
+# to run
+# python ex8.py one, two, three
+
